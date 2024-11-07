f661c12b6ffb852e6fcb2a94b2f6435042254d3f
patchuru prasad <patchuru.prasad@TL215.triconhome.local>
2024-11-07 15:04:33 +0530

    twod arrays

---
+++ twodarrays.js
@@ -0,0 +1,8 @@
+//declaration and intilization of twod arrays
+var twod = [[22, 33], [44, 55]];
+console.log(twod[0]);
+for (var i = 0; i < twod.length; i++) {
+    for (var j = 0; j < twod.length; j++) {
+        console.log(twod[i][j]);
+    }
+}
---
+++ twodarrays.ts
@@ -0,0 +1,10 @@
+//declaration and intilization of twod arrays
+let twod: number[][] = [[22, 33], [44, 55]];
+console.log(twod[0]); //printing values using loop and for loop
+for (let i = 0; i < twod.length; i++) {
+    for (let j = 0; j < twod.length; j++) {
+        console.log(twod[i][j]);
+    }
+}
+
+
