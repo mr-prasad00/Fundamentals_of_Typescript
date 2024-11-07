fe083d46ec870251e03353b9ba6f960aef353270
patchuru prasad <patchuru.prasad@TL215.triconhome.local>
2024-11-07 14:35:25 +0530

    array declaration

---
+++ arrays.js
@@ -0,0 +1,6 @@
+//array declaration and intilzation
+var num = [22, 33, 44, 55];
+//or by using type as well we can declare array
+var num2 = [12, 13, 14, 15, 16];
+console.log(num);
+console.log(num2);
